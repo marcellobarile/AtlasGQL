0a256bc6dc8a770df29746a4df75411a1996a093
Marcello Barile <marcello_barile@rcomext.com>
2021-01-11 16:39:10 +0100

    prettified

--- app.ts
+++ app.ts
@@ -1,5 +1,16 @@
-import { Common } from './server/helpers/common';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import logger from 'morgan';
+import multer from 'multer';
+import request from 'request-promise-native';
+
 import { Conf } from './config/common';
+import { GraphQlServer } from './server';
+import { Common } from './server/helpers/common';
+import restRoutes from './server/routes/rest';
 
 // Store some confs
 Conf.ServerAddr = process.env.ADDR || Conf.DefaultAppAddr;
@@ -8,20 +19,9 @@ Conf.ServerEnv = process.env.NODE_TARGET_ENV || Common.constants.ENV_DEV;
 Conf.ServerEnvId = process.env.NODE_ENV_ID || 1;
 Conf.ServerKey = `${Conf.AppName}:${Conf.ServerEnv}-${Conf.ServerEnvId}`;
 
-const isDev = Conf.ServerEnv === Common.constants.ENV_DEV
-  || Conf.ServerEnv === Common.constants.ENV_TEST;
-
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import express from 'express';
-import logger from 'morgan';
-import multer from 'multer';
-import bodyParser from 'body-parser';
-import request from 'request-promise-native';
-
-import restRoutes from './server/routes/rest';
-import { GraphQlServer } from './server';
+const isDev =
+  Conf.ServerEnv === Common.constants.ENV_DEV ||
+  Conf.ServerEnv === Common.constants.ENV_TEST;
 
 const app = express();
 
@@ -41,12 +41,7 @@ app.options(Conf.AcceptedDomains, cors(corsOptions));
 // -------------------
 //  Apollo Server
 // -------------------
-GraphQlServer.createServer(
-  Conf.GraphQlPath,
-  app,
-  corsOptions,
-  isDev,
-);
+GraphQlServer.createServer(Conf.GraphQlPath, app, corsOptions, isDev);
 
 if (isDev) {
   // Enabling Request module debug
@@ -55,9 +50,11 @@ if (isDev) {
 
 // Configure multer to accept multiple files (.any())
 // or a single file (.single('file'))
-app.use(multer({
-  storage: multer.memoryStorage(),
-}).any());
+app.use(
+  multer({
+    storage: multer.memoryStorage(),
+  }).any()
+);
 
 // ------------------------
 // REST
@@ -74,9 +71,11 @@ app.set('view engine', Conf.ViewsEngine);
 app.use(logger('dev'));
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 
 export { app };
